c43c32317ac5e69eb5d3c29ade052ef2de49c0bf
Ibrahim Azez <ibrahimazez@protonmail.com>
2024-06-28 12:40:25 +0300

    feat: schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { ClsModule } from 'nestjs-cls';
 
 import { GlobalModule } from './core/modules/global/global.module';
+import { PrismaService } from './core/modules/global/services/prisma.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [GlobalModule],
+  imports: [
+    ClsModule.forRoot({
+      global: true,
+      middleware: { mount: false },
+      guard: { mount: true },
+      plugins: [
+        new ClsPluginTransactional({
+          imports: [
+            // module in which the PrismaClient is provided
+            GlobalModule,
+          ],
+          adapter: new TransactionalAdapterPrisma({
+            // the injection token of the PrismaClient
+            prismaInjectionToken: PrismaService,
+          }),
+        }),
+      ],
+    }),
+    GlobalModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 
+import { envConfig } from './common/utils/env-config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envConfig.PORT);
 }
-bootstrap();
+void bootstrap();
